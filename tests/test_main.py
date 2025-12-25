a14d42e8a71f92447756c59daa0886fb2225bdd1
Idchaoudi-Youssef <toumiyoussef857@gmail.com>
2025-12-25 15:10:18 +0100

    Ajout du pipeline CI/CD et des tests

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from main import greet
+
+def test_greet(capsys):
+    # Appel de la fonction à tester
+    greet("Alice")
+    
+    # Capture de la sortie standard (ce qui est affiché dans le terminal)
+    captured = capsys.readouterr()
+    
+    # Vérification (Assertion)
+    assert captured.out == "Bonjour, Alice!\n"
